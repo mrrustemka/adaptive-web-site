e173b88ad632026c39c615984e1e0505c3cab7c6
mrrustemka <mubarakshin1996@gmail.com>
2024-08-03 19:01:25 +0400

    created-header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.scss';
+import basket from './images/BASKET.svg';
+import logo from './images/LOGO.svg'
 
 function App() {
   return (
     <div>
+      <header className='header'>
+        <img id='logo'src={logo} alt='Logo'></img>
+        <a className='header-home' href="#">Home</a>
+        <a className='header-products' href="#">Products</a>
+        <a  href="#"><img className='header-basket-icon' src={basket} alt='Basket'/></a>
+      </header>
+      <div></div>
+      <footer></footer>
     </div>
   );
 }
