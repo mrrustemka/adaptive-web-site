e3c0c9a7538beb585c8628cf2bb69b2492aa99bb
mrrustemka <mubarakshin1996@gmail.com>
2024-08-04 14:28:27 +0400

    creaetd-header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import './App.scss';
 import basket from './images/BASKET.svg';
 import logo from './images/LOGO.svg'
+import mobileMenu from './images/MENU.svg'
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 360);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 360);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div>
-      <header className='header'>
-        <img id='logo'src={logo} alt='Logo'></img>
-        <a className='header-home' href="#">Home</a>
-        <a className='header-products' href="#">Products</a>
-        <a  href="#"><img className='header-basket-icon' src={basket} alt='Basket'/></a>
+      <header id="banner" >
+        <div className='header'>
+          <img id='logo'src={logo} alt='Logo'></img>
+          <a className='header-home' href="#">Home</a>
+          <a className='header-products' href="#">Products</a>
+          <a className='header-basket' href="#">
+            <img className='header-basket-icon' src={isMobile ? mobileMenu : basket} alt='Basket'/>
+          </a>
+        </div>
+        <div className='banner-header'>
+          <h1>Discover the vast expanses of <span>space</span></h1>
+          <h2>Where the possibilities are <span>endless!</span></h2>
+          <button>Learn more</button>
+        </div>
       </header>
+      <div>
+
+      </div>
       <div></div>
       <footer></footer>
     </div>
